97a04fe6cf710c33f2b15089a28e6c0130c42175
seondongpyo <seondongpyo@gmail.com>
2021-08-02 20:33:21 +0900

    initial commit

---
+++ src/main/java/com/example/springpropertiesdemo/SpringPropertiesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpropertiesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPropertiesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPropertiesDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springpropertiesdemo/SpringPropertiesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpropertiesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPropertiesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
